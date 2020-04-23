e1cb55efdff56b6535f15dc4006fb7c1549e903a
onedoit <37774781+onedoit@users.noreply.github.com>
2020-04-23 16:52:34 +0300

    enble other shape

--- src/tetris.go
+++ src/tetris.go
@@ -1,5 +1,6 @@
 package main
 
+// https://github.com/idoroze/Tetris_Base_Text
 //  ________________       ______________      ________________    _________           __        ________
 // |                |     |              |    |                |  |   ___   \         |  |      /        \
 //  ¯¯¯¯¯|   |¯¯¯¯¯¯      |  |¯¯¯¯¯¯¯¯¯¯¯      ¯¯¯¯¯|   |¯¯¯¯¯¯   |  |   |   |         ¯¯      /  \¯¯¯¯\  \
@@ -11,6 +12,7 @@ package main
 //        ¯¯¯              ¯¯¯¯¯¯¯¯¯¯¯¯¯¯            ¯¯¯           ¯¯¯      ¯¯¯        ¯¯       ¯¯¯¯¯¯¯¯¯¯
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"os/exec"
 	"time"
@@ -60,10 +62,12 @@ type deBug struct {
 func init() {
 	edge()
 	Delay = 3000 * time.Millisecond
+
 }
 
 func main() {
 	fmt.Println("ready")
+
 	T := time.Now()
 	x := [8]int{5, 0, 6, 0, 5, 1, 6, 1}
 	bob := newobj(x)
@@ -87,10 +91,10 @@ func main() {
 
 		bob.lastplace = bob.place
 		bob.dir = dir(r)
-		go view(bob)
+		view(bob)
 		fmt.Printf("%v , %v\n", time.Since(T), Delay)
 		if (time.Since(T)) >= Delay {
-			down(bob)
+			go down(bob)
 			T = time.Now()
 		}
 
@@ -143,12 +147,6 @@ func edge() {
 }
 
 func newobj(p [8]int) *obj {
-	//Iblock = [8]int{5, 0, 6, 0, 5, 1, 6, 1}
-	//jblock = [8]int{5, 0, 6, 0, 5, 1, 5, 2}
-	//lblock = [8]int{5, 0, 4, 0, 5, 1, 5, 2}
-	//sblock
-	//Tblock = [8]int{5, 0, 4, 0, 6, 0, 5, 1}
-	//zblock
 	name := obj{p, dir(0), p, false, nil}
 
 	return &name
@@ -157,7 +155,7 @@ func newobj(p [8]int) *obj {
 func add(b *obj) {
 	move(b)
 	for out := 0; out < 8; out++ {
-		if b.place[out] > 21 { // can walk tour the edge
+		if b.place[out] > 21 { // can't walk tour the edge
 			b.place = b.lastplace
 		}
 	}
@@ -170,11 +168,11 @@ func add(b *obj) {
 
 	for i := 0; i < 8; i += 2 {
 
-		if Bord[b.place[i+1]][b.place[i]] != 1 {
+		if Bord[b.place[i+1]][b.place[i]] == 0  {
 			Bord[b.place[i+1]][b.place[i]] = 2
 		} else {
 
-			if b.place[i+1] >= 20 {
+			if b.place[i+1] >= 20 || Bord[b.place[i+1]][b.place[i]] == 3 {
 				b.d = true
 			}
 
@@ -198,10 +196,18 @@ func del(bob *obj) {
 		Bord[bob.place[i+1]][bob.place[i]] = 0
 		Bord[bob.lastplace[i+1]][bob.lastplace[i]] = 0
 	}
+	if len(bob.dead) != 0 {
+		for i := 0; i < len(bob.dead); i += 2 {
+			Bord[bob.dead[i+1]][bob.dead[i]] = 0
+		}
+	}
+
 }
 
 func move(bob *obj) {
 	switch int(bob.dir) {
+	case 119, 87: //up change pos
+		change(bob)
 	case 83, 115: //down
 
 		if bob.place[1] < 20 && bob.place[3] < 20 && bob.place[5] < 20 && bob.place[7] < 20 {
@@ -233,7 +239,16 @@ func move(bob *obj) {
 }
 
 func (bop *obj) newlook() {
-	bop.place = [8]int{5, 0, 6, 0, 5, 1, 6, 1}
+	Shape := map[int][8]int{
+		0: [8]int{5, 0, 6, 0, 5, 1, 6, 1}, //shape cube
+		1: [8]int{5, 0, 6, 0, 5, 1, 5, 2}, //shape r
+		2: [8]int{5, 0, 4, 0, 5, 1, 5, 2}, //shape j
+		3: [8]int{5, 0, 6, 0, 1, 5, 1, 4}, //shape S
+		4: [8]int{5, 0, 4, 0, 6, 0, 5, 1}, //shape L
+		5: [8]int{5, 0, 4, 0, 1, 5, 1, 6}, //shape Z
+	}
+
+	bop.place = Shape[rand.Intn(6)]
 }
 
 //debug
@@ -299,5 +314,8 @@ func down(bob *obj) {
 	} else {
 		bob.place = bob.lastplace
 	}
+
+}
+func change(bob *obj) {
 
 }
